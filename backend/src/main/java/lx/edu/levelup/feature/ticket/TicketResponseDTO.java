28b41c7f81ea06bfc26ef6cdafa732a44f5aaa3a
suhyunnnnnni <dastar998176@gmail.com>
2025-09-11 21:10:36 +0900

    ticketVo/DTO/Mapper mapper-ticket.xml 수정

--- backend/src/main/java/lx/edu/levelup/feature/ticket/TicketResponseDTO.java
+++ backend/src/main/java/lx/edu/levelup/feature/ticket/TicketResponseDTO.java
@@ -1,9 +1,23 @@
 package lx.edu.levelup.feature.ticket;
 
-/*
- * domain에서 필요한 정보만 뽑아서 프론트에 전달
- */
 
 public class TicketResponseDTO {
-
+	
+	private Long ticket_no;
+	private String ticket_name;
+	private String ticket_reserve_date;
+	private int adult;
+	private int children;
+	
+	public static TicketResponseDTO fromEntity(lx.edu.levelup.feature.ticket.domain.TicketVO ticket) {
+		TicketResponseDTO dto = new TicketResponseDTO();
+		dto.ticket_no = ticket.getTicket_no();
+		dto.ticket_name = ticket.getTicket_name();
+		dto.ticket_reserve_date = ticket.getTicket_reserve_date();
+		dto.adult = ticket.getAdult();
+		dto.children = ticket.getChildren();
+		return dto;
+	}
+	
+	
 }
--- backend/src/main/java/lx/edu/levelup/feature/ticket/domain/TicketVO.java
+++ backend/src/main/java/lx/edu/levelup/feature/ticket/domain/TicketVO.java
@@ -4,40 +4,45 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
-@Entity
-public class TicketEntity {
+public class TicketVO {
 
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
 	private Long ticket_no;
-	
 	private String ticket_name;
 	private String ticket_reserve_date;
-	// 성인티켓 구매수량
-	private int adult;
-	// 아동티켓 구매수량
-	private int children;
+	private int adult;  	// 성인티켓 구매수량
+	private int children;  	// 아동티켓 구매수량
 	
 	
+	// 생성자
+	public TicketVO(Long ticket_no, String ticket_name, String ticket_reserve_date, int adult, int children) {
+		this.ticket_no = ticket_no;
+		this.ticket_name = ticket_name;
+		this.ticket_reserve_date = ticket_reserve_date;
+		this.adult = adult;
+		this.children = children;
+	}
 	
+	// getter & setter
 	public Long getTicket_no() {
 		return ticket_no;
 	}
+
 	public String getTicket_name() {
 		return ticket_name;
 	}
+
 	public String getTicket_reserve_date() {
 		return ticket_reserve_date;
 	}
+
 	public int getAdult() {
 		return adult;
 	}
+
 	public int getChildren() {
 		return children;
 	}
-	
 
-	
-	
 }
