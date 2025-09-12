ed06865d744a7d8d7ccdfedd5522d71d5727ee8a
kimminchae1 <minchaeg10@gmail.com>
2025-09-12 23:44:15 +0900

    user 수정

--- backend/src/main/java/lx/edu/levelup/feature/ticket/TicketResponseDTO.java
+++ backend/src/main/java/lx/edu/levelup/feature/ticket/TicketResponseDTO.java
@@ -1,25 +1,25 @@
-package lx.edu.levelup.feature.ticket;
-
-import lombok.Getter;
-
-@Getter
-public class TicketResponseDTO {
-	
-	private Long ticket_no;
-	private String ticket_name;
-	private String ticket_reserve_date;
-	private int ticket_amount;  	
-	private String ticket_money_status;  	
-	
-	public static TicketResponseDTO fromEntity(lx.edu.levelup.feature.ticket.domain.TicketVO ticket) {
-		TicketResponseDTO dto = new TicketResponseDTO();
-		dto.ticket_no = ticket.getTicket_no();
-		dto.ticket_name = ticket.getTicket_name();
-		dto.ticket_reserve_date = ticket.getTicket_reserve_date();
-		dto.ticket_amount = ticket.getTicket_amount();
-		dto.ticket_money_status = ticket.getTicket_money_status();
-		return dto;
-	}
-	
-	
-}
+//package lx.edu.levelup.feature.ticket;
+//
+//import lombok.Getter;
+//
+//@Getter
+//public class TicketResponseDTO {
+//	
+//	private Long ticket_no;
+//	private String ticket_name;
+//	private String ticket_reserve_date;
+//	private int ticket_amount;  	
+//	private String ticket_money_status;  	
+//	
+//	public static TicketResponseDTO fromEntity(lx.edu.levelup.feature.ticket.domain.TicketVO ticket) {
+//		TicketResponseDTO dto = new TicketResponseDTO();
+//		dto.ticket_no = ticket.getTicket_no();
+//		dto.ticket_name = ticket.getTicket_name();
+//		dto.ticket_reserve_date = ticket.getTicket_reserve_date();
+//		dto.ticket_amount = ticket.getTicket_amount();
+//		dto.ticket_money_status = ticket.getTicket_money_status();
+//		return dto;
+//	}
+//	
+//	
+//}
--- backend/src/main/java/lx/edu/levelup/feature/user/domain/UserVO.java
+++ backend/src/main/java/lx/edu/levelup/feature/user/domain/UserVO.java
@@ -5,8 +5,8 @@ import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
-@Data// @Getter @Setter @ToString@EqualsAndHashCode, @RequiredArgsConstructor
-@NoArgsConstructor // 파라미터가 없는 기본 생성자 생성
+@Data// @Getter @Setter @ToString @EqualsAndHashCode, @RequiredArgsConstructor
+@NoArgsConstructor // 매개변수 없는 기본 생성자 생성
 @AllArgsConstructor // 모든 필드를 파라미터로 받는 생성자 생성
 public class UserVO {
 
