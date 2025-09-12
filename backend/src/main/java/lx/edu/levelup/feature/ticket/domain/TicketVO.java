c85dda212d5fc16ba6ef3decf02e17218e792df6
suhyunnnnnni <dastar998176@gmail.com>
2025-09-12 18:20:45 +0900

    ticket insert, select 기능 구현

--- backend/src/main/java/lx/edu/levelup/feature/ticket/domain/TicketVO.java
+++ backend/src/main/java/lx/edu/levelup/feature/ticket/domain/TicketVO.java
@@ -5,44 +5,20 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.Table;
+import lombok.Data;
 
+@Data
 public class TicketVO {
-
-	private Long ticket_no;
-	private String ticket_name;
-	private String ticket_reserve_date;
-	private int adult;  	// 성인티켓 구매수량
-	private int children;  	// 아동티켓 구매수량
+	
+	private Long ticketNo;
+    private int userNo;
+    private String ticketName;
+    private String ticketReserveDate;
+    private int ticketAmount;  	
+    private String ticketMoneyStatus;;  	
 	
 	
-	// 생성자
-	public TicketVO(Long ticket_no, String ticket_name, String ticket_reserve_date, int adult, int children) {
-		this.ticket_no = ticket_no;
-		this.ticket_name = ticket_name;
-		this.ticket_reserve_date = ticket_reserve_date;
-		this.adult = adult;
-		this.children = children;
-	}
 	
-	// getter & setter
-	public Long getTicket_no() {
-		return ticket_no;
-	}
 
-	public String getTicket_name() {
-		return ticket_name;
-	}
-
-	public String getTicket_reserve_date() {
-		return ticket_reserve_date;
-	}
-
-	public int getAdult() {
-		return adult;
-	}
-
-	public int getChildren() {
-		return children;
-	}
 
 }
