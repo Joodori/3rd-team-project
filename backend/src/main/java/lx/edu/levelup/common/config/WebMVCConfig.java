62819ae2739df855e87a27243a07642ebc9a9ac5
kimminchae1 <minchaeg10@gmail.com>
2025-09-13 20:27:47 +0900

    session 코드 삭제 및 user 수정

--- backend/src/main/java/lx/edu/levelup/common/config/WebMVCConfig.java
+++ backend/src/main/java/lx/edu/levelup/common/config/WebMVCConfig.java
@@ -4,12 +4,9 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
-import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
-
-import lx.edu.levelup.common.interceptor.Interceptor;
 
 @Configuration
 @EnableWebMvc
@@ -23,19 +20,6 @@ public class WebMVCConfig implements WebMvcConfigurer {
 		resolver.setSuffix(".jsp");
 		return resolver;
 	}
-
-	@Bean
-	public Interceptor loginInterceptor() {
-		return new Interceptor();
-		
-	}
-	
-/*	@Override
-	public void addInterceptors(InterceptorRegistry registry) {
-        registry.addInterceptor(loginInterceptor())
-                .addPathPatterns("/**") // 모든 URL에 적용하되,
-                .excludePathPatterns("/login", "/resources/**"); // 로그인 페이지와 리소스 파일은 제외
-	}*/
 	
 	@Override
 	public void addResourceHandlers(ResourceHandlerRegistry registry) {
