88124479a2800f5de5868ffa940ea58ff4554500
suhyunnnnnni <dastar998176@gmail.com>
2025-09-14 23:19:00 +0900

    ride: 예약 기능, 마이페이지 기능 구현

--- backend/src/main/java/lx/edu/levelup/feature/ride/RideRepository.java
+++ backend/src/main/java/lx/edu/levelup/feature/ride/RideRepository.java
@@ -1,9 +1,14 @@
 package lx.edu.levelup.feature.ride;
 
+import java.util.List;
+
 import org.apache.ibatis.annotations.Mapper;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Mapper
 public interface RideRepository {
 	
 	void insertRideBookConfirm(RideVO vo);
+	
+	List<RideResponseDTO> getRideBookList(@RequestParam("user_no") int user_no);
 }
